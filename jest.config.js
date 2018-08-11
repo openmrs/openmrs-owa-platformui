77988063c477247e2ed008946f8bd53b4fdb1fdb
Sibo Donald <sibo.donald@gmail.com>
2018-08-12 00:42:14 +0300

    PLAT-41: Rename the platformui owa route urls

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__tests__/__mocks__/fileMock.js"
   },
-  testPathIgnorePatterns: ["<rootDir>/__tests__/__mocks__/fileMock.js"]
+  testPathIgnorePatterns: ["<rootDir>/__tests__/__mocks__/fileMock.js"],
+  testURL: 'http://localhost'
 }
