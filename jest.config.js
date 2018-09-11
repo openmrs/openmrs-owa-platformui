008c30d0bab156d329d0af2f596e39b8d21c47aa
dkayiwa <dkayiwa@openmrs.org>
2018-09-11 17:50:30 +0300

    Revert "PLAT-56: Properly format the jest config file (#17)"
    This reverts commit 1edee6483500b0439d419e597917076690bc7002.

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__tests__/__mocks__/fileMock.js',
+      '<rootDir>/__tests__/__mocks__/fileMock.js'
   },
-  testPathIgnorePatterns: ['<rootDir>/__tests__/__mocks__/fileMock.js'],
+  testPathIgnorePatterns: ["<rootDir>/__tests__/__mocks__/fileMock.js"],
   testURL: 'http://localhost',
   'setupTestFrameworkScriptFile': './setupTests.js',
   collectCoverageFrom: ['**/app/**/*.js', '**/app/**/*.jsx'],
