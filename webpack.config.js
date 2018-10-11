a5af5ed5092bc67208c24f0b95f0f05be2eab6e7
Sibo Donald <sibo.donald16@gmail.com>
2018-10-11 14:46:14 +0300

    PLAT-67: Add webpack.config file to the Platformui application

---
+++ webpack.config.js
@@ -0,0 +1,219 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+// generated on 2017-09-01 using generator-openmrs-owa 0.6.0
+
+const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs');
+const env = require('yargs').argv.mode;
+const target = require('yargs').argv.target;
+const targetPort = require('yargs').argv.targetPort;
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WebpackOnBuildPlugin = require('on-build-webpack');
+
+const UglifyPlugin = webpack.optimize.UglifyJsPlugin;
+const CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+const DedupePlugin = webpack.optimize.DedupePlugin;
+
+const nodeModulesDir = path.resolve(__dirname, '../node_modules');
+
+require.extensions['.webapp'] = function(module, filename) {
+  module.exports = fs.readFileSync(filename, 'utf8');
+};
+const manifest = require('./app/manifest.webapp');
+
+const THIS_APP_ID = 'openmrs-owa-platformui';
+
+const THIS_APP_VERSION = JSON.parse(manifest).version;
+
+let plugins = [];
+const nodeModules = {};
+
+let outputFile = `.bundle`;
+let vendorOutputFile;
+let outputPath;
+
+let devtool;
+
+let getConfig = function() {
+  let config;
+
+  try {
+    // look for config file
+    config = require('./config.json');
+  } catch (err) {
+    // create file with defaults if not found
+    config = {
+      LOCAL_OWA_FOLDER: '/Users/dondrzzy/openmrs/server1/owa/',
+      APP_ENTRY_POINT:
+        'http://localhost:8080/openmrs/owa/openmrs-owa-platformui/index.html'
+    };
+
+    fs.writeFile('config.json', JSON.stringify(config));
+  } finally {
+    return config;
+  }
+};
+let config = getConfig();
+
+/** Minify for production */
+if (env === 'production') {
+  plugins.push(
+    new UglifyPlugin({
+      output: {
+        comments: false
+      },
+      minimize: true,
+      sourceMap: false,
+      compress: {
+        warnings: false
+      }
+    })
+  );
+  plugins.push(new DedupePlugin());
+  outputFile = `${outputFile}.min.js`;
+  vendorOutputFile = 'vendor.bundle.js';
+  outputPath = `${__dirname}/dist/`;
+  plugins.push(
+    new WebpackOnBuildPlugin(function(stats) {
+      //create zip file
+      let archiver = require('archiver');
+      let output = THIS_APP_VERSION
+        ? fs.createWriteStream(`${THIS_APP_ID}-${THIS_APP_VERSION}.zip`)
+        : fs.createWriteStream(`${THIS_APP_ID}.zip`);
+      let archive = archiver('zip');
+
+      output.on('close', function() {
+        console.log(
+          'distributable has been zipped! size: ' + archive.pointer()
+        );
+      });
+      archive.on('error', function(err) {
+        throw err;
+      });
+      archive.pipe(output);
+      archive.directory(`${outputPath}`, '');
+      archive.finalize();
+    })
+  );
+} else if (env === 'deploy') {
+  outputFile = `${outputFile}.js`;
+  vendorOutputFile = 'vendor.bundle.js';
+  outputPath = `${config.LOCAL_OWA_FOLDER}${THIS_APP_ID}`;
+  devtool = 'source-map';
+} else if (env === 'dev') {
+  outputFile = `${outputFile}.js`;
+  vendorOutputFile = 'vendor.bundle.js';
+  outputPath = `${__dirname}/dist/`;
+  devtool = 'source-map';
+}
+
+plugins.push(
+  new BrowserSyncPlugin({
+    proxy: {
+      target: config.APP_ENTRY_POINT
+    }
+  })
+);
+
+plugins.push(
+  new CommonsChunkPlugin({
+    name: 'vendor',
+    filename: vendorOutputFile
+  })
+);
+
+plugins.push(
+  new HtmlWebpackPlugin({
+    template: './app/index.html',
+    inject: 'body'
+  })
+);
+
+plugins.push(
+  new CopyWebpackPlugin([
+    {
+      from: './app/manifest.webapp'
+    }
+  ])
+);
+
+plugins.push(
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify('production')
+    }
+  })
+);
+
+plugins.push(
+  new CopyWebpackPlugin([
+    {
+      from: './app/img/',
+      to: 'img'
+    }
+  ])
+);
+
+let webpackConfig = {
+  quiet: false,
+  entry: {
+    app: `${__dirname}/app/js/openmrs-owa-platformui`,
+    css: `${__dirname}/app/css/openmrs-owa-platformui.css`,
+    vendor: ['react', 'react-router']
+  },
+  devtool: devtool,
+  target,
+  output: {
+    path: outputPath,
+    filename: '[name]' + outputFile
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: [
+            'transform-es2015-destructuring',
+            'transform-object-rest-spread',
+            'transform-class-properties'
+          ],
+          cacheDirectory: true
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        loader: 'url'
+      },
+      {
+        test: /\.html$/,
+        loader: 'html'
+      }
+    ]
+  },
+  resolve: {
+    root: path.resolve('./src'),
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins,
+  externals: nodeModules
+};
+
+module.exports = webpackConfig;
